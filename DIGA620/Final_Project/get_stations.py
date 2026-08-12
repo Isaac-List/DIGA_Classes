8b645fe3ac68d15058bad0a927f91c12ec446bf4
Isaac List <48964096+Isaac-List@users.noreply.github.com>
2026-08-12 14:24:22 -0500

    Merge both get_stations scripts and allow for choosing to search by ZIP or FIPS

--- DIGA620/Final_Project/get_stations.py
+++ DIGA620/Final_Project/get_stations.py
@@ -6,7 +6,7 @@ token = noaa_token.token
 # Station data endpoint
 data_endpoint = "https://www.ncei.noaa.gov/cdo-web/api/v2/stations"
 
-def get_stations(zips: list) -> dict:
+def get_stations_zip(zips: list) -> dict:
     """Get a list of stations that meet requirements"""
     # Return object
     stations = dict()
@@ -37,19 +37,65 @@ def get_stations(zips: list) -> dict:
     # Return results
     return stations
 
+def get_stations_fips(fips: str) -> dict:
+    """Get a list of stations that meet requirements"""
+    # Return object
+    stations = dict()
+
+    # Parameters for weather data retrieval
+    parameters = {
+        "locationid": f"FIPS:{fips}",
+        "sortfield": "name",
+        "sortorder": "asc",
+        "startdate": "2026-01-01",
+        "enddate": "2026-03-31",
+        "datatypeid": ["PRCP", "TMAX", "TMIN"],
+        "limit": "1000"
+    }
+
+    # Request
+    station_data = requests.get(data_endpoint, params = parameters, headers = token)
+
+    # Look at results
+    station_json = station_data.json()
+    station_results = station_json.get("results", [])
+
+    # Select results with >= 99% data coverage
+    for i in range(len(station_results)):
+        if float(station_results[i]["datacoverage"]) >= 0.999:
+            stations[station_results[i]["name"]] = station_results[i]["id"]
+
+    # Return results
+    return stations
+
 def main():
-    zip_codes = []
+    choice: str = input("Choose to search by ZIP codes (1) or FIPS number (2): ")
 
-    print("Enter a zip code to search, type STOP when done.")
-    zip_code = input("ZIP code: ")
-    while zip_code != "STOP":
-        zip_codes.append(zip_code)
+    if choice == "1":
+        # From ZIP Codes
+        zip_codes = []
+
+        print("Enter a zip code to search, type STOP when done.")
         zip_code = input("ZIP code: ")
-    
-    station_ids: dict = get_stations(zip_codes)
+        while zip_code != "STOP":
+            zip_codes.append(zip_code)
+            zip_code = input("ZIP code: ")
 
-    with open("station_ids.json", "w") as output:
-        json.dump(station_ids, output)    
+        station_ids_from_zip: dict = get_stations_zip(zip_codes)
+
+        with open("station_ids_from_zip.json", "w") as output:
+            json.dump(station_ids_from_zip, output)
+
+    # From FIPS code
+    elif choice == "2":
+        fips = input("Enter your state's FIPS code: ")
+        station_ids_from_fips: dict = get_stations_fips(fips)
+
+        with open("station_ids_from_fips.json", "w") as output:
+            json.dump(station_ids_from_fips, output)
+
+    else:
+        print("Invalid choice selection.")
 
 if __name__ == "__main__":
     main()
